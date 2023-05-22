f7588b31019b941a063f2c898c6c9a589eeb6dea
cla6shade <cla6shade@gmail.com>
2023-05-23 00:49:58 +0900

    로고 클릭하면 사라지도록 구현

--- js/intro.js
+++ js/intro.js
@@ -1,4 +1,9 @@
 let intro = {}
+
+const title = document.querySelector(".film-title");
+const subtitle = document.querySelector(".film-subtitle");
+const icon = document.querySelector(".main-icon");
+
 intro.openCurtain = () => {
     let rightOpener = document.querySelector(".opener-left");
     let leftOpener = document.querySelector(".opener-right");
@@ -28,6 +33,7 @@ intro.openCurtain = () => {
         intro.showTitle();
     }, 1800);
 }
+
 intro.showTitle = () => {
     let keyFrames_icon = [
         {
@@ -67,9 +73,6 @@ intro.showTitle = () => {
         easing: "ease-in",
         fill: "forwards"
     }
-    let title = document.querySelector(".film-title");
-    let subtitle = document.querySelector(".film-subtitle");
-    let icon = document.querySelector(".main-icon");
 
     icon.animate(keyFrames_icon, options);
     options.delay = 300;
@@ -77,5 +80,51 @@ intro.showTitle = () => {
     title.animate(keyFrames_title, options);
     options.delay = 500;
     subtitle.animate(keyFrames_subtitle, options);
+
+    let icon_rotate = [
+        {transform: "rotate(0deg)"},
+        {transform: "rotate(360deg)"}
+    ]
+    let rotate_options = {
+        delay: 700,
+        duration: 1000,
+        easing: "ease-out",
+        iterations: Infinity
+    }
+
+    icon.animate(icon_rotate, rotate_options);
+
+    icon.onclick = intro.onLogoClick;
+}
+
+intro.onLogoClick = () => {
+    let keyFrames_vanish = [
+        {opacity: 1},
+        {opacity: 0},
+    ]
+    let vanish_options = {
+        delay: 700,
+        duration: 1000,
+        easing: "ease-in",
+        fill: "forwards"
+    }
+    let curtains = document.getElementsByClassName("curtain");
+    let curtain_left = curtains.item(0);
+    let curtain_right = curtains.item(1);
+    let keyFrames_curtain_left = [
+        {transform: "translate(0, 0)"},
+        {transform: "translate(-360px, 0)"}
+    ]
+    let keyFrames_curtain_right = [
+        {transform: "translate(0, 0)"},
+        {transform: "translate(360px, 0)"}
+    ]
+
+    curtain_left.animate(keyFrames_curtain_left, vanish_options);
+    curtain_right.animate(keyFrames_curtain_right, vanish_options);
+
+    title.animate(keyFrames_vanish, vanish_options);
+    subtitle.animate(keyFrames_vanish, vanish_options);
+    icon.animate(keyFrames_vanish, vanish_options);
 }
 export default intro;
