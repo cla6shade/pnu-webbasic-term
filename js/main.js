c8d5b2071aeb67939ff966222778830a0e17d35c
cla6shade <cla6shade@gmail.com>
2023-06-01 17:01:52 +0900

    박스오피스 구현 완료

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function getPosterUrl(movieCd) {
 function requestDailyBoxOffice(targetDt) {
     return new Promise((resolve, reject) => {
         const url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json";
-        $.ajax(url + "?key=" + key + "&targetDt=" + targetDt).done(async data => {
+        $.ajax(url + "?key=" + key + "&targetDt=" + targetDt + "&itemPerPage=" + 10).done(async data => {
             let boxOffice = data.boxOfficeResult.dailyBoxOfficeList;
             for (let movie of boxOffice) {
                 let movieCd = movie.movieCd;
@@ -43,15 +43,80 @@ function onClickDaily() {
     let dateInputs =
         document.querySelectorAll(".date-input-container input[type='number']");
     let dates = []
-    dateInputs.forEach((e) =>{
+    dateInputs.forEach((e) => {
         dates.push(parseInt(e.value));
     })
     let [year, month, date] = dates;
-    if(year < 2000 || month < 1 || month > 12 || date < 1 || date > 31){
+    if (! this.isValidDate(year, month, date)) {
         alert("잘못된 날짜입니다. 확인 후 다시 시도해주세요.");
         return;
     }
-    getDailyBoxOffice(year, month, date).then((boxOffice) =>{
+    if(this.isSameOrAfterToday(year, month, date)){
+        alert("금일 기준 하루 이전 날짜부터 조회가 가능합니다.");
+        return;
+    }
 
+    setDailyLoadingStatus(true);
+    getDailyBoxOffice(year, month, date).then((boxOffice) => {
+        setDailyLoadingStatus(false);
+        displayDailyBoxOffice(boxOffice);
     })
+}
+
+function displayDailyBoxOffice(boxOffice){
+    const container = document.querySelector(".box-office-container");
+    container.innerHTML = "";
+    for(let movie of boxOffice){
+        let posterUrl = movie.posterUrl;
+        let movieArea = document.createElement("div");
+        movieArea.classList.add("movie-area");
+
+        let poster = document.createElement("img");
+        poster.classList.add("poster");
+        poster.setAttribute("src", posterUrl);
+        let title = document.createElement("h4");
+        title.innerText = movie.movieNm + " (" + movie.rank + "위)";
+
+        let info = document.createElement("p");
+        info.innerText = "개봉일: " + movie.openDt;
+
+        movieArea.appendChild(poster);
+        movieArea.appendChild(title);
+        movieArea.appendChild(info);
+
+        container.appendChild(movieArea);
+    }
+}
+
+function setDailyLoadingStatus(loading = true) {
+    let spin = document.getElementById("daily-loading");
+    if (loading) {
+        spin.style.display = "block";
+    } else {
+        spin.style.display = "none";
+    }
+}
+
+function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+function isSameOrAfterToday(year, month, day){
+    month = parseInt(month) - 1
+    const date = year.toString() + "-" + month.toString() + "-" + day.toString();
+    const nowDate = new Date();
+
+    const dateString = nowDate.getFullYear().toString() + "-" +
+        nowDate.getMonth().toString() + "-" + nowDate.getDate().toString();
+    return dateString <= date;
+}
+
+function isValidDate(year, month, day) {
+    const date = new Date(year, month - 1, day);
+    if (date.getFullYear() === year &&
+        date.getMonth() === month - 1 &&
+        date.getDate() === day) {
+        return !(month === 2 && day === 29 && !isLeapYear(year));
+    } else {
+        return false;
+    }
 }
---
+++ js/transition.js
@@ -0,0 +1,59 @@
+const HOME = 0;
+const DAILY = 1;
+const YEARLY = 2;
+const THEATERS = 3;
+const selectors = [".title-container", ".daily-container",
+    ".yearly-container", ".theaters-container"];
+
+let now = HOME;
+let availableState = true;
+
+/**
+ * @param {number} id
+ */
+function transfer(id){
+    if(availableState && now !== id){
+        availableState = false;
+        console.log(id)
+        vanish(300);
+        appear(id, 300, 300);
+        now = id;
+    }
+}
+function appear(id, delay, duration){
+    const appearContainer = document.querySelector(selectors[id]);
+    setTimeout(()=>{
+        appearContainer.style.display = "flex";
+    }, delay)
+    let keyframes = [
+        {opacity: "0"},
+        {opacity: "1"},
+    ];
+    let options = {
+        duration: duration,
+        delay: delay,
+        fill: "forwards",
+        easing: "ease-out"
+    }
+    appearContainer.animate(keyframes, options);
+    setTimeout(() => {
+        availableState = true;
+    }, delay + duration);
+}
+function vanish(duration){
+    const nowContainer = document.querySelector(selectors[now]);
+    let keyframes = [
+        {opacity: "1"},
+        {opacity: "0"},
+    ];
+    let options = {
+        duration: duration,
+        delay: 0,
+        fill: "forwards",
+        easing: "ease-out"
+    }
+    nowContainer.animate(keyframes, options);
+    setTimeout(() => {
+        nowContainer.style.display = "none";
+    }, duration);
+}
